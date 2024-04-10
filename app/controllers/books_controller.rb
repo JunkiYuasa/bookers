40fb1cc913aa758c74371e28a903c77682a530ad
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-04-10 15:22:57 +0000

    [Progress_2]投稿機能、一覧画面を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    flash[:notice] = "Successfully posted"
+    redirect_to books_path
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  # ストロングパラメーター
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
